dae16078a1efe5c26cac606a0c591944d47427de
DheerajChetti <143454847+DheerajChetti@users.noreply.github.com>
2023-10-12 15:21:41 -0500

    Update ThermalPlantDisease.py
    Updated Code for the Plant Disease prediction model with the updated thermal dataset

--- ThermalPlantDisease.py
+++ ThermalPlantDisease.py
@@ -1,41 +1,95 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import warnings
+warnings.filterwarnings('ignore')
 
-# Load and preprocess the new dataset
-# Replace 'new_dataset_path' with the path to your new dataset
-new_dataset_path = '/Users/dheerajchetti/Downloads'
-new_data_generator = ImageDataGenerator(
-    rescale=1.0/255,  # Normalize pixel values to [0, 1]
-    validation_split=0.2
-)
 
-new_train_generator = new_data_generator.flow_from_directory(
-    new_dataset_path,
-    target_size=(224, 224),  # Adjust to match your model's input size
-    batch_size=32,
-    class_mode='categorical',  # Adjust depending on your problem
-    subset='training'
-)
+# Step 1: Load the existing dataset
+existing_dataset = pd.read_csv('thermal_images.csv')
 
-new_val_generator = new_data_generator.flow_from_directory(
-    new_dataset_path,
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical',
-    subset='validation'
-)
+# Step 2: Load the new dataset
+new_dataset = pd.read_csv('thermal_images.csv')
 
-# Load your existing model
-existing_model = keras.models.load_model('existing_model.h5')
+# Step 3: Merge the new dataset with the existing dataset
+updated_dataset = pd.concat([plant_disease, thermal_images], axis=0, ignore_index=True)
 
-# Fine-tune the model on the new data
-history = existing_model.fit(
-    new_train_generator,
-    epochs=10,  # Adjust the number of epochs as needed
-    validation_data=new_val_generator
-)
+# Step 4: Save the updated dataset if necessary
+updated_dataset.to_csv('updated_PlantDisease.csv', index=False)
 
-# Save the updated model
-existing_model.save('updated_model.h5')
+
+# Step 5: Split the data into train and test sets
+trainset, testset = train_test_split(data, test_size=0.2)
+
+# Step 6: Train the recommendation model
+model = KNNBasic()
+model.fit(trainset)
+
+
+
+df = pd.read_csv('/Users/dheerajchetti/Downloads/updated_PlantDisease.csv')
+df.head()
+df.tail()
+df.size
+df.shape
+df.columns
+df['label'].unique()
+df.dtypes
+df['label'].value_counts()
+#sns.heatmap(df.corr(),annot=True)
+features = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+target = df['label']
+# Initialzing empty lists to append all model's name and corresponding name
+acc = []
+model = []
+# Splitting into train and test data
+
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(features,target,test_size = 0.2,random_state =2)
+from sklearn.tree import DecisionTreeClassifier
+
+DecisionTree = DecisionTreeClassifier(criterion="entropy",random_state=2,max_depth=5)
+
+DecisionTree.fit(Xtrain,Ytrain)
+
+predicted_values = DecisionTree.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Decision Tree')
+print("DecisionTrees's Accuracy is: ", x*100)
+
+print(classification_report(Ytest,predicted_values))
+from sklearn.model_selection import cross_val_score
+# Cross validation score (Decision Tree)
+score = cross_val_score(DecisionTree, features, target,cv=5)
+score
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+DT_pkl_filename = 'C:/Users/chandana/Desktop/Harvestify/models/DecisionTree.pkl'
+# Open the file to save as pkl file
+DT_Model_pkl = open(DT_pkl_filename, 'wb')
+pickle.dump(DecisionTree, DT_Model_pkl)
+# Close the pickle instances
+DT_Model_pkl.close()
+from sklearn.naive_bayes import GaussianNB
+
+NaiveBayes = GaussianNB()
+
+NaiveBayes.fit(Xtrain,Ytrain)
+
+predicted_values = NaiveBayes.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Naive Bayes')
+print("Naive Bayes's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+# Cross validation score (NaiveBayes)
+score = cross_val_score(NaiveBayes,features,target,cv=5)
+score
 
